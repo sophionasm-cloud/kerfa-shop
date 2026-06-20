dc7e66b46ce6564266f12edfa4924f4f071900ac
sophionasm-cloud <sophionasm@gmail.com>
2026-06-20 11:03:07 +0300

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'https://YOUR-GITHUB-USERNAME.github.io'],
+    'allowed_origins' => [
+        'http://localhost:5173', 
+        'http://localhost:5174', 
+        'https://app-45694296-daec-4264-9167-45a65f5e680e.cleverapps.io'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
